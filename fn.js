aea1f77ec7046057648a2129e03d31e8eda2f1fc
hurryhuang1007 <hurryhuang1007@qq.com>
2019-05-22 14:42:32 +0800

    let some test

--- fn.js
+++ fn.js
@@ -7,34 +7,9 @@ exports.getInfos = async function (url, fn) {
     o[i] = img.get(i)
     return o
   }, {})
-
-  // let infos = ['openslide.level-count', 'slide-associated-images',
-  //   'openslide.level[0].height',
-  //   'openslide.level[1].height',
-  //   'openslide.level[2].height',
-  //   'openslide.level[3].height',
-  //   'openslide.level[4].height',
-  // ].reduce((o, i) => {
-  //   o[i] = img.get(i)
-  //   return o
-  // }, {})
-  console.log(infos)
-  // fn(JSON.stringify(infos.width))
-  // fn()
+  // console.log(infos)
+  fn(infos)
 }
-
-// try {
-//   !function () {
-//     let img = vips.Image.openslideload('C:/data/test_svs/slide-2019-02-18T10-15-45-R2-S11.mrxs')
-//     let infos = img.getFields().reduce((o, i) => {
-//       o[i] = img.get(i)
-//       return o
-//     }, {})
-//     console.log(infos)
-//   }()
-// } catch (e) {
-//   console.log(e)
-// }
 
 exports.getThumbnail = async function (url, fn) {
   let img = await openslideload(url, { associated: 'thumbnail' })
--- renderer.js
+++ renderer.js
@@ -2,8 +2,7 @@ const { remote } = require('electron')
 const fn = remote.require('./fn')
 window.fn = fn
 
-const url = 'C:/data/test_svs/slide-2019-02-18T10-15-45-R2-S11.mrxs'
-// fn.getInfos(url, a => window.a = a)
+const url = 'C:/data/Mirax2-Fluorescence-2.mrxs'
 fn.getThumbnail(url, async buffer => {
   // window.buffer = buffer
   let imgBit = await createImageBitmap(new Blob([buffer], { type: 'image/jpeg' }))
@@ -14,38 +13,8 @@ fn.getThumbnail(url, async buffer => {
   imgBit.close()
 })
 
-// console.time('缩略图加载')
-// try {
-//   vips.Image.openslideload(url, {
-//     associated: 'thumbnail',
-//     // async: async (e, img) => {
-//     //   if (e) return console.error('缩略图加载失败')
-//     //   console.timeEnd('缩略图加载')
-//     //   let buffer = img.writeToBuffer('.jpg')
-//     //   let imgBit = await createImageBitmap(new Blob([buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength)], { type: 'image/jpeg' }))
-//     //   // let imgBit = await createImageBitmap(buffer)
-//     //   document.querySelector('#canvas').getContext('2d').drawImage(imgBit, 0, 0)
-//     //   imgBit.close()
-//     //   console.log('!!!')
-//     // }
-//   })
-// } catch (e) {
-//   console.log('error')
-// }
-
-// setTimeout(async () => {
-//   console.time('get buffer')
-//   let buffer = remote.getGlobal('buffer')
-//   console.timeEnd('get buffer')
-//   let imgBit = await createImageBitmap(new Blob([buffer], { type: 'image/jpeg' }))
-//   let canvas = document.querySelector('#canvas')
-//   canvas.width = 0.995 * window.innerWidth
-//   canvas.height = 0.995 * window.innerHeight
-//   canvas.getContext('2d').drawImage(imgBit, 0, 0, canvas.width, canvas.height)
-//   imgBit.close()
-//   console.log('!!!')
-// }, 1000);
-
-// console.time('get array')
-// remote.getGlobal('array')
-// console.timeEnd('get array')
+setTimeout(() => {
+  alert('getInfos will be start')
+  fn.getInfos(url, console.log)
+  alert('done')
+}, 5000)
